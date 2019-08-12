9fd5bb5d67b822f80e19652e24180ae619952b7f
zhanglin <zhanglin@zhanglindeMacBook-Air.local>
2019-08-12 17:31:00 +0800

    小程序关于

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="wrapper">
     <section class="main-intro">
-      <img class="intro-pic" src />
+      <img class="intro-pic" :src="picUrl"/>
       <p class="intro-name">{{name}}</p>
       <p class="intro-des">{{description}}</p>
+    </section>
+    <ul class="main-list">
+      <li class="list-1">
+        <label>服务类别</label>
+        <span>{{serviceClass}}</span>
+      </li>
+      <li class="list-2" @click="goMainInfo">
+        <label>主体信息</label>
+        <span>{{mainInfo}}</span>
+      </li>
+      <li class="list-3">
+        <label>客服电话</label>
+        <a :href="'tel:'+serviceTel">{{serviceTel}}</a>
+      </li>
+    </ul>
+    <section class="add-like">
+      <div class="like-btn" :class="{'like': likeStatus == '1'}" @click="addLike"></div>
+      <p>为该小程序点赞</p>
     </section>
   </div>
 </template>
@@ -19,7 +37,18 @@ export default {
       serviceClass: '金融服务', //服务类别
       mainInfo: '平安科技（深圳）有限公司', //主体信息
       serviceTel:'95511', //客服电话
-      likeStatus: localStorage.getItem('likeFlag') || 0, //点赞状态 1-已赞，0-未赞
+      likeStatus: localStorage.getItem('likeFlag') || '0', //点赞状态 1-已赞，0-未赞
+    }
+  },
+  methods: {
+    addLike () { //点赞
+      if(this.likeStatus != '1'){
+        localStorage.setItem('likeFlag', '1')
+        this.likeStatus = '1'
+      }
+    },
+    goMainInfo () { //跳转主体信息
+      this.$router.push('/mainInfo')
     }
   }
 }
@@ -51,6 +80,69 @@ export default {
   .intro-des {
     padding-top: 20px;
     @include sc(26px, #666);
+  }
+}
+.main-list {
+  margin-top: 20px;
+  .list-1,
+  .list-2,
+  .list-3 {
+    position: relative;
+    @include wh(100vw, 88px);
+    padding: 0 40px;
+    background: #fff;
+    & > label {
+      @include sc(30px, #000);
+      line-height: 88px;
+    }
+    & > span{
+      padding-left: 60px;
+      line-height: 88px;
+      @include sc(30px, #a8a8a8);
+    }
+    & > a {
+      padding-left: 60px;
+      line-height: 88px;
+      @include sc(30px, #1177ed);
+    }
+  }
+  .list-1::after {
+    display: block;
+    content: '';
+    width: calc(100vw - 40px);
+    height: 1Px;
+    background: #f1f1f1;
+  }
+  .list-2::after {
+    position: absolute;
+    right: 40px;
+    top: 36px;
+    display: inline-block;
+    content: '';
+    @include wh(15px, 15px);
+    border-right: 3px solid #cdcdcd;
+    border-bottom: 3px solid #cdcdcd;
+    transform: rotate(-45deg);
+  }
+  .list-3 {
+    margin-top: 20px;
+  }
+}
+.add-like {
+  margin-top: 124px;
+  .like-btn {
+    margin: 0 auto;
+    @include wh(120px, 120px);
+    transition: opacity .5s;
+    @include background-cover('../../assets/icon_like0.png');
+    &.like {
+      @include background-cover('../../assets/icon_like1.png');
+    }
+  }
+  & > p {
+    margin-top: 10px;
+    text-align: center;
+    @include sc(24px, #a8a8a8);
   }
 }
 </style>
--- src/views/advice/advice.vue
+++ src/views/advice/advice.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    advice
+  <div class="wrapper">
+    <img class="advice-img" src="../../assets/advice.png"/>
+    <p class="advice-text">功能建设中，敬请期待</p>
   </div>
 </template>
 
@@ -12,5 +13,19 @@ export default {
 
 
 <style lang="scss" scoped>
-
+@import '../../style/mixin.scss';
+.wrapper {
+  @include wh(100vw, auto);
+  .advice-img {
+    display: block;
+    margin-top: 220px;
+    margin-left: 120px;
+    @include wh(400px, 370px);
+  }
+  .advice-text {
+    margin-top: 60px;
+    @include sc(30px, #b8b8b8);
+    text-align: center;
+  }
+}
 </style>
